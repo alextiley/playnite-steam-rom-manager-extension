e0438f26bd360f76d12d671fc656c4ba01f947bd
Alex Tiley <1131174+alextiley@users.noreply.github.com>
2023-11-17 22:37:25 +0000

    feat: implement a start-up script for monitoring game start and end

--- Source/Scripts/launch.cmd
+++ Source/Scripts/launch.cmd
@@ -1,88 +1,80 @@
 @echo off
-
-REM ---------------------------------------------------------------------------------
-REM   Playnite SteamRomManagerCompanion Game Session Launcher
-REM ---------------------------------------------------------------------------------
-REM   The Playnite SteamRomManagerCompanion extension allows you to launch or install
-REM   all of your non-Steam games from Steam, desktop or big picture mode. Simply
-REM   install the extension, perform a library scan and then launch your games from
-REM   Steam!
-REM
-REM   This script acts as a launcher for Playnite game sessions.
-REM
-REM     How it works:
-REM       * It starts a Playnite process if one is not already running.
-REM       * It then triggers a Playnite game launch session, or installer.
-REM         - In the background, the extension writes a file to disk to indicate
-REM           that a game install or launch session is active.
-REM         - The extension removes the file when the game install or launch session
-REM           ends.
-REM       * It monitors for when the install or game session ends and exits the
-REM         process in order to notify Steam that the game session has ended.
-REM       * Steam will then update it's game play status. This means that your
-REM         friends will be able to see what you're playing, even if it's not a
-REM         Steam game!
-REM
-REM     How to use:
-REM       * Install the Playnite SteamRomManagerCompanion extension.
-REM       * Perform a library scan.
-REM       * Launch a game from Steam.
-REM
-REM     Notes:
-REM       * If you wish to run this script manually, do so at your own risk.
-REM       * This script must be run from the Playnite installation directory,
-REM         otherwise it will likely fail.
-REM ---------------------------------------------------------------------------------
-REM   Arguments:
-REM     [ExtensionId]           : The Playnite SteamRomManagerCompanion extension ID.
-REM     [GameId]                : The Playnite game ID to launch.
-REM     [DryRun]                : Optional. If set to "dry-run", the script will not
-REM                               actually launch the game session. This is useful
-REM                               for testing.
-REM
-REM     The script's working directory must be the Playnite installation directory.
-REM ---------------------------------------------------------------------------------
-REM   Exit Codes:
-REM     0                       : Success.
-REM     1                       : Invalid arguments.
-REM     2                       : Playnite executable not found in current directory.
-REM     3                       : Failed to start Playnite process.
-REM     4                       : Failed to start game session.
-REM ---------------------------------------------------------------------------------
-
-REM Function to display usage instructions
-:ShowUsage
-echo Usage: %0 [ExtensionId] [GameId]
-
-REM Function to display an error message and exit
-:ShowErrorAndExit
-echo Error: %1
-if "%3" == "usage" call :ShowUsage
-exit /b %2
+:: cd %LocalAppData%\Playnite && %UserProfile%\Code\playnite-steam-rom-manager-extension\Source\Scripts\launch.cmd 12345
+:: ---------------------------------------------------------------------------------
+::   Playnite SteamRomManagerCompanion Game Session Launcher
+:: ---------------------------------------------------------------------------------
+::   The Playnite SteamRomManagerCompanion extension allows you to launch or install
+::   all of your non-Steam games from Steam, desktop or big picture mode. Simply
+::   install the extension, perform a library scan and then launch your games from
+::   Steam!
+::
+::   This script acts as a launcher for Playnite game sessions.
+::
+::     How it works:
+::       * It starts a Playnite process if one is not already running.
+::       * It then triggers a Playnite game launch session, or installer.
+::         - In the background, the extension writes a file to disk to indicate
+::           that a game install or launch session is active.
+::         - The extension removes the file when the game install or launch session
+::           ends.
+::       * It monitors for when the install or game session ends and exits the
+::         process in order to notify Steam that the game session has ended.
+::       * Steam will then update it's game play status. This means that your
+::         friends will be able to see what you're playing, even if it's not a
+::         Steam game!
+::
+::     How to use:
+::       * Install the Playnite SteamRomManagerCompanion extension.
+::       * Perform a library scan.
+::       * Launch a game from Steam.
+::
+::     Notes:
+::       * If you wish to run this script manually, do so at your own risk.
+::       * This script must be run from the Playnite installation directory,
+::         otherwise it will likely fail.
+:: ---------------------------------------------------------------------------------
+::   Arguments:
+::     [GameId]                : The Playnite game ID to launch.
+::     [DryRun]                : Optional. If set to "dry-run", the script will not
+::                               actually launch the game session. This is useful
+::                               for testing.
+::
+::     The script's working directory must be the Playnite installation directory.
+:: ---------------------------------------------------------------------------------
+::   Exit Codes:
+::     0                       : Success.
+::     1                       : Invalid arguments.
+::     2                       : Playnite executable not found in current directory.
+::     3                       : Failed to start Playnite process.
+::     4                       : Failed to start game session.
+:: ---------------------------------------------------------------------------------
 
 echo Starting Playnite SteamRomManagerCompanion game session...
 
-REM ---------------------------------------------------------------------------------
-REM   Validate the provided arguments
-REM ---------------------------------------------------------------------------------
+:: ---------------------------------------------------------------------------------
+::   Validate the provided arguments
+:: ---------------------------------------------------------------------------------
 
 echo Validating arguments...
 
 if "%~1" == "" (
-    call :ShowErrorAndExit "ExtensionId argument not specified. Exiting." 1 "usage"
-) else if "%~2" == "" (
-    call :ShowErrorAndExit "GameId argument not specified. Exiting." 1 "usage"
+    echo Error: GameId argument not specified. Exiting.
+    echo Usage: %~0 GameId [dry-run]
+    exit /b 1
 )
 
 if "%~3" == "dry-run" (
-    @echo on
     set "IsDryRun=1"
 	echo Dry run mode enabled. No actions will be performed.
+) else (
+    set "IsDryRun=0"
 )
 
+echo Arguments appear to be valid.
+
+set "GameId=%~1"
 set "PlayniteDirectory=%cd%"
-set "ExtensionId=%~1"
-set "GameId=%~2"
+set "ExtensionId=5fe1d136-a9dc-44d7-80d2-43c02df6e546"
 set "PlayniteDesktopProcess=Playnite.DesktopApp.exe"
 set "PlayniteFullscreenProcess=Playnite.FullscreenApp.exe"
 set "PlayniteLaunchPath=%PlayniteDirectory%\%PlayniteDesktopProcess%"
@@ -92,12 +84,15 @@ set "GameStateFileToMonitor=%PlayniteDirectory%\ExtensionData\%ExtensionId%\stat
 echo Validating working directory...
 
 if not exist "%PlayniteDirectory%\%PlayniteDesktopProcess%" (
-    call :ShowErrorAndExit "Playnite executable not found in the current directory. Exiting." 2 "usage"
+    echo Error: Playnite executable not found in %PlayniteDirectory%\%PlayniteDesktopProcess%. Exiting.
+    exit /b 2
 )
 
-REM ---------------------------------------------------------------------------------
-REM   Check if a Playnite process is already running
-REM ---------------------------------------------------------------------------------
+echo Working directory is valid: %PlayniteDirectory%.
+
+:: ---------------------------------------------------------------------------------
+::   Check if a Playnite process is already running
+:: ---------------------------------------------------------------------------------
 
 echo Checking if Playnite is already running...
 
@@ -107,44 +102,53 @@ set "PlayniteDesktopProcessMissing=%ERRORLEVEL%"
 tasklist /FI "IMAGENAME eq %PlayniteFullscreenProcess%" 2>NUL | find /I /N "%PlayniteFullscreenProcess%">NUL
 set "PlayniteFullscreenProcessMissing=%ERRORLEVEL%"
 
-REM ---------------------------------------------------------------------------------
-REM   Start the Playnite process if not already running and delay for 5 seconds
-REM ---------------------------------------------------------------------------------
-
-echo Starting Playnite process if not already running...
-
 if "%PlayniteDesktopProcessMissing%" equ "0" (
-    echo %PlayniteDesktopProcess% is already running.
+	echo %PlayniteDesktopProcess% is already running.
     set "PlayniteStartedByScript=0"
 ) else if "%PlayniteFullscreenProcessMissing%" equ "0" (
-    echo %PlayniteFullscreenProcess% is already running.
+	echo %PlayniteFullscreenProcess% is already running.
     set "PlayniteStartedByScript=0"
 ) else (
-    echo Playnite is not running. Starting process.
-    if "%DryRun%" equ "0" (
-        powershell -Command "Start-Process %PlayniteLaunchPath% -ArgumentList %PlayniteLaunchFlags%" || call :ShowErrorAndExit "Failed to start Playnite process." 3
-	)
+    echo Playnite is not running. Starting process: %PlayniteLaunchPath% %PlayniteLaunchFlags%
     set "PlayniteStartedByScript=1"
-    timeout /t 5 /nobreak >nul
 )
 
-REM ---------------------------------------------------------------------------------
-REM   Start the game session and delay for 5 seconds
-REM ---------------------------------------------------------------------------------
+:: ---------------------------------------------------------------------------------
+::   Start the Playnite process if not already running and delay for 5 seconds
+:: ---------------------------------------------------------------------------------
 
-echo Starting game session...
+if "%PlayniteStartedByScript%" equ "1" (
 
-if "%DryRun%" equ "0" (
-	powershell -Command "Start-Process 'playnite://steam-launcher/%GameId%'" || call :ShowErrorAndExit "Failed to start game session." 4
+    if "%IsDryRun%" equ "0" (
+        echo Running powershell command...
+        powershell -Command "Start-Process %PlayniteLaunchPath% -ArgumentList ""%PlayniteLaunchFlags%""" || (echo Failed to start Playnite process. && exit /b 3)
+	)
+    timeout /t 5 /nobreak >nul
+
+    echo Playnite process started.
+)
+
+:: ---------------------------------------------------------------------------------
+::   Start the game session and delay for 5 seconds
+:: ---------------------------------------------------------------------------------
+
+echo Starting game session via playnite://steam-launcher/%GameId%...
+
+if "%IsDryRun%" equ "0" (
+    echo Running powershell command...
+	powershell -Command "Start-Process playnite://steam-launcher/%GameId%" || (echo Failed to start game session && exit /b 4)
 )
 
 timeout /t 5 /nobreak >nul
 
-REM ---------------------------------------------------------------------------------
-REM   Monitor the game session and wait for it to end
-REM ---------------------------------------------------------------------------------
+echo Game session started.
 
-echo Monitoring game session and waiting for it to end...
+:: ---------------------------------------------------------------------------------
+::   Monitor the game session and wait for it to end
+:: ---------------------------------------------------------------------------------
+
+echo Game state file is %GameStateFileToMonitor%.
+echo Monitoring game state file and waiting for it's removal...
 
 :MonitorGameSession
 if exist "%GameStateFileToMonitor%" (
@@ -152,18 +156,24 @@ if exist "%GameStateFileToMonitor%" (
     goto :MonitorGameSession
 )
 
-REM ---------------------------------------------------------------------------------
-REM   Clean up and kill the Playnite process if it was started by this script
-REM ---------------------------------------------------------------------------------
+echo Game session has ended.
 
-if "%PlayniteStartedByScript%" equ "1" if "%DryRun%" equ "0" (
+:: ---------------------------------------------------------------------------------
+::   Clean up and kill the Playnite process if it was started by this script
+:: ---------------------------------------------------------------------------------
+::
+:: TODO consider if we really need to do this.
+::
+if "%PlayniteStartedByScript%" equ "1" if "%IsDryRun%" equ "0" (
+    echo Exiting Playnite process.
 	taskkill /f /im "%PlayniteDesktopProcess%" >nul
+    echo Playnite successfully exited.
 )
 
-REM ---------------------------------------------------------------------------------
-REM   Tell Steam that the game session has ended by exiting this script
-REM ---------------------------------------------------------------------------------
+:: ---------------------------------------------------------------------------------
+::   Tell Steam that the game session has ended by exiting this script
+:: ---------------------------------------------------------------------------------
 
-echo Game session ended. Exiting.
+echo Exiting script. Goodbye.
 
 exit /b
